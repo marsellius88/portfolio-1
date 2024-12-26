eb9802cdda7f20bf68dcadb6bfc9f9876feea2fd
marsellius88 <marselliustan1@gmail.com>
2024-12-26 14:12:26 +0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,48 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import RootLayout from "./layouts/RootLayout";
+import Home from "./pages/Home";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+      light: "#63a4ff",
+      dark: "#004ba0",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: "#9c27b0",
+      light: "#d05ce3",
+      dark: "#6a0080",
+      contrastText: "#ffffff",
+    },
+    background: {
+      default: "#f4f6f8",
+      paper: "#ffffff",
+    },
+  },
+});
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Box, Button } from "@mui/material";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>
+        Tombol Secondary
+      </Button>
+    </div>
+  );
+}
