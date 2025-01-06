81a158c0cf990ea62e4ca280cbf854c547de4c86
marsellius88 <marselliustan1@gmail.com>
2025-01-06 21:29:17 +0700

    memperbaiki kesalahan penulisan impor

--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import { Box } from "@mui/material";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import DrawerAppBar from "../components/DrawerAppbar";
+import DrawerAppBar from "../components/DrawerAppBar";
 
 export default function RootLayout() {
   return (
